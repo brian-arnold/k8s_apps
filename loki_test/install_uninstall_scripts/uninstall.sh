3e2ceadd058987d2321f4a8983f962f56b906efe
Brian Arnold <brianjohnarnold@gmail.com>
2025-10-12 20:28:55 -0700

    Update loki uninstall script

--- loki_test/install_uninstall_scripts/uninstall.sh
+++ loki_test/install_uninstall_scripts/uninstall.sh
@@ -1,9 +1,11 @@
 # UNINSTALL LOKI
 helm uninstall loki -n loki
-kubectl delete pvc --all -n loki
-kubectl delete pv --all -n loki
 kubectl delete namespace loki  # This removes any remaining resources
 
 # UNINSTALL ALLOY
 helm uninstall alloy -n alloy
 kubectl delete namespace alloy
+
+# DO NOT RUN THESE!
+# kubectl delete pvc --all -n loki
+# kubectl delete pv --all -n loki
